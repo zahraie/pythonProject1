49762a9dcca1cb45d6844fd9f8297f4fdb1b1400
zahra sohrabi <zahra.sohrabi.1374@gmail.com>
2023-02-10 15:31:39 +0330

    first commit

---
+++ neuraspike/config.py
@@ -0,0 +1,17 @@
+# import the necessary packages
+import os
+
+# initialize the path to the root folder where the dataset resides and the
+# path to the train and test directory
+DATASET_FOLDER = f'dataset'
+TRAIN_DIRECTORY = os.path.join(DATASET_FOLDER, "train")
+TEST_DIRECTORY = os.path.join(DATASET_FOLDER, "test")
+
+# initialize the amount of samples to use for training and validation
+TRAIN_SIZE = 0.90
+VAL_SIZE = 0.10
+
+# specify the batch size, total number of epochs and the learning rate
+BATCH_SIZE = 16
+NUM_OF_EPOCHS = 50
+LR = 1e-1
